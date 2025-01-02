7f8659d69411a45ade7d96724604a7b2d5beaf23
Mayowa <Mostcharming920@yahoo.com>
2025-01-02 19:45:16 +0100

    protected routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,32 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import Index from './pages/home'
+import { useContent } from './services/useContext'
+
+function ProtectedRoute({
+  element,
+  roles,
+  authState
+}: {
+  element: JSX.Element
+  roles: string[]
+  authState: any
+}) {
+  if (!authState?.isAuthenticated || !authState?.user) {
+    return <Navigate to='/' replace />
+  }
+
+  const userRole = authState.user.role.toLowerCase()
+  if (roles.includes(userRole)) {
+    return element
+  }
+
+  return <Navigate to='/' replace />
+}
 
 function App() {
-  // const { authState } = useContent()
+  const { authState } = useContent()
+
   return (
     <>
       <ToastContainer
@@ -18,10 +41,46 @@ function App() {
       />
       <Routes>
         <Route path='/' element={<Index />} />
-        <Route path='/auth/*' element={<Index />} />
-        <Route path='/admin/*' element={<Index />} />
-        <Route path='/donor/*' element={<Index />} />
-        <Route path='/ngo/*' element={<Index />} />
+        <Route
+          path='/auth/*'
+          element={
+            <ProtectedRoute
+              element={<Index />}
+              roles={['admin', 'donor', 'corporate', 'ngo']}
+              authState={authState}
+            />
+          }
+        />
+        <Route
+          path='/admin/*'
+          element={
+            <ProtectedRoute
+              element={<Index />}
+              roles={['admin']}
+              authState={authState}
+            />
+          }
+        />
+        <Route
+          path='/donor/*'
+          element={
+            <ProtectedRoute
+              element={<Index />}
+              roles={['donor', 'corporate']}
+              authState={authState}
+            />
+          }
+        />
+        <Route
+          path='/ngo/*'
+          element={
+            <ProtectedRoute
+              element={<Index />}
+              roles={['ngo']}
+              authState={authState}
+            />
+          }
+        />
         <Route path='*' element={<Navigate to='/' replace />} />
       </Routes>
     </>
