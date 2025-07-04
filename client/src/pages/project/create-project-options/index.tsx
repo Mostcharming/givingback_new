38802ea21428a7f2de1d4ef9472aaea6d895c2e6
mostcharming <mostcharming920@yahoo.com>
2025-07-04 18:01:48 +0100

    project page for the NGOs

--- client/src/pages/project/create-project-options/index.tsx
+++ client/src/pages/project/create-project-options/index.tsx
@@ -2,7 +2,14 @@ import { ChevronRight, CircleCheckBig } from "lucide-react";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Select from "react-select";
-import { Button, FormGroup, Input, InputGroup } from "reactstrap";
+import {
+  Button,
+  FormGroup,
+  Input,
+  InputGroup,
+  InputGroupAddon,
+  InputGroupText,
+} from "reactstrap";
 import useBackendService from "../../../services/backend_service";
 import { useContent } from "../../../services/useContext";
 
@@ -11,6 +18,7 @@ const CreateProject = () => {
   const { authState } = useContent();
   const role = authState.user?.role;
   const [formData, setFormData] = useState({
+    //main details
     title: "",
     description: "",
     status: "",
@@ -21,6 +29,7 @@ const CreateProject = () => {
     orgemail: "",
     cost: null,
     raised: null,
+    //milestones
   });
   const [step, setStep] = useState(1);
   const [areas, setAreas] = useState([]);
@@ -189,6 +198,293 @@ const CreateProject = () => {
                   setFormData((prev) => ({
                     ...prev,
                     title: e.target.value,
+                  }))
+                }
+              />
+            </InputGroup>
+          </FormGroup>
+          <FormGroup className="mt-4">
+            <InputGroup className="input-group-alternative">
+              <Input
+                style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                className="p-3"
+                placeholder="Project description"
+                type="textarea"
+                name="description"
+                rows={5}
+                required
+                value={formData.description}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    description: e.target.value,
+                  }))
+                }
+              />
+            </InputGroup>
+          </FormGroup>
+          <div className="row mt-4">
+            <div className="col-md-6">
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <select
+                    style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                    className="form-control p-3"
+                    name="status"
+                    required
+                    value={formData.status}
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        status: e.target.value,
+                      }))
+                    }
+                  >
+                    <option value="" disabled>
+                      Project Status
+                    </option>
+                    <option value="active">Active</option>
+                    <option value="completed">Completed</option>
+                  </select>
+                </InputGroup>
+              </FormGroup>
+            </div>
+
+            <div className="col-md-6">
+              <div className="row">
+                <div className="col-md-6">
+                  <FormGroup>
+                    <label className="mb-1">Start date</label>
+
+                    <InputGroup className="input-group-alternative">
+                      <Input
+                        style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                        className="p-3"
+                        placeholder="Project duration"
+                        type="date"
+                        name="duration"
+                        rows={5}
+                        required
+                        value={formData.startDate}
+                        onChange={(e) =>
+                          setFormData((prev) => ({
+                            ...prev,
+                            startDate: e.target.value,
+                          }))
+                        }
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                </div>
+                <div className="col-md-6">
+                  <FormGroup>
+                    <label className="mb-1">End date</label>
+                    <InputGroup className="input-group-alternative">
+                      <Input
+                        style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                        className="p-3"
+                        placeholder="Project duration"
+                        type="date"
+                        name="endDate"
+                        rows={5}
+                        required
+                        value={formData.endDate}
+                        onChange={(e) =>
+                          setFormData((prev) => ({
+                            ...prev,
+                            endDate: e.target.value,
+                          }))
+                        }
+                      />
+                    </InputGroup>
+                  </FormGroup>
+                </div>
+              </div>
+            </div>
+          </div>
+          <FormGroup className="mt-4">
+            <InputGroup className="input-group-alternative">
+              <Select
+                styles={{
+                  control: (provided) => ({
+                    ...provided,
+                    backgroundColor: "#F2F2F2",
+                    minHeight: "100%",
+                    height: "100%",
+                  }),
+                  valueContainer: (provided) => ({
+                    ...provided,
+                    height: "100%",
+                    padding: "0 6px",
+                  }),
+                  input: (provided) => ({
+                    ...provided,
+                    margin: "0px",
+                  }),
+                  indicatorsContainer: (provided) => ({
+                    ...provided,
+                    height: "55px",
+                  }),
+                }}
+                className="w-100"
+                placeholder="Select related fields"
+                required
+                onChange={onThematicAreaChange}
+                options={areas.map((category) => ({
+                  value: category.name,
+                  label: category.name,
+                }))}
+                isMulti
+              />
+            </InputGroup>
+          </FormGroup>
+          <div className="row">
+            <div className="col-md-6">
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <Input
+                    style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                    className="p-3"
+                    placeholder="Project cost (NGN)"
+                    type="number"
+                    name="cost"
+                    rows={5}
+                    required
+                    value={formData.cost}
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        cost: e.target.value,
+                      }))
+                    }
+                  />
+                  <InputGroupAddon addonType="append">
+                    <InputGroupText
+                      style={{
+                        backgroundColor: "#F2F2F2",
+                        height: "100%",
+                      }}
+                    >
+                      | NGN
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+              </FormGroup>
+            </div>
+            <div className="col-md-6">
+              <FormGroup>
+                <InputGroup className="input-group-alternative">
+                  <Input
+                    style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                    className="p-3"
+                    placeholder="Amount raised (NGN)"
+                    type="number"
+                    name="raised"
+                    rows={5}
+                    required
+                    value={formData.raised}
+                    onChange={(e) =>
+                      setFormData((prev) => ({
+                        ...prev,
+                        raised: e.target.value,
+                      }))
+                    }
+                  />
+                  <InputGroupAddon addonType="append">
+                    <InputGroupText
+                      style={{
+                        backgroundColor: "#F2F2F2",
+                        height: "100%",
+                      }}
+                    >
+                      | NGN
+                    </InputGroupText>
+                  </InputGroupAddon>
+                </InputGroup>
+              </FormGroup>
+            </div>
+          </div>
+          <FormGroup className="">
+            <InputGroup className="input-group-alternative">
+              <Input
+                style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                className="p-3"
+                placeholder="Project manager's email"
+                type="email"
+                name="orgemail"
+                required
+                value={formData.orgemail}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    orgemail: e.target.value,
+                  }))
+                }
+              />
+            </InputGroup>
+          </FormGroup>
+
+          <div className="d-flex justify-content-between pt-4">
+            <button
+              onClick={handleExit}
+              type="button"
+              className="btn btn-outline-success px-5 py-3"
+            >
+              Exit
+            </button>
+            <Button
+              onClick={handleNextStep}
+              className="btn px-5 py-3"
+              disabled={
+                !formData.title ||
+                !formData.description ||
+                !formData.status ||
+                !formData.duration ||
+                !formData.endDate ||
+                !formData.cost ||
+                !formData.raised ||
+                !formData.orgemail ||
+                !formData.interest_area
+              }
+              style={{
+                border: "none",
+                background: "#02a95c",
+              }}
+            >
+              Continue
+            </Button>
+          </div>
+        </form>
+      </div>
+    );
+  };
+  const renderMilestones = () => {
+    return (
+      <div className="">
+        <div className="mb-4">
+          <h2 style={{ color: "black" }} className="h4 fw-bold mb-2">
+            Timeline & Milestone
+          </h2>
+          <p className="text-muted mb-0">
+            Add details about completing the project
+          </p>
+        </div>
+
+        <form className="pr-5">
+          <FormGroup className="">
+            <InputGroup className="input-group-alternative">
+              <Input
+                style={{ backgroundColor: "#F2F2F2", height: "100%" }}
+                className="p-3"
+                placeholder="Milestone title"
+                type="text"
+                name="title"
+                required
+                // value={formData.milestone}
+                onChange={(e) =>
+                  setFormData((prev) => ({
+                    ...prev,
+                    milestone: e.target.value,
                   }))
                 }
               />
